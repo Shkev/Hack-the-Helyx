a591a721b95f8b872e5a36291a1033968b96d185
Shayan Azmoodeh <shkev.az@gmail.com>
2020-04-11 12:53:49 -0700

    adding calcPrice

---
+++ calcPrice.py
@@ -0,0 +1,8 @@
+"""Takes in the following as input:
+* Price of charger provider's electricity per kWh
+* Power output of provider in kW
+* Time that the user wants to charge for
+
+Outputs:
+* Price to charge the customer for charging car
+"""
